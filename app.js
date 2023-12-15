a6a2bde4aa21aeed71b6806088b08bc2d4c2a49f
Leonardo Astros <leonardoastros@gmail.com>
2023-12-15 15:47:46 -0400

    Other practices

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
+var childElement = /** @class */ (function () {
+    function childElement(n) {
+        this.index = n;
+        this.tagName = "h".concat(n);
+    }
+    childElement.prototype.element = function () {
+        var el = document.createElement(this.tagName);
+        el.textContent = "text for child ".concat(this.index);
+        return el;
+    };
+    return childElement;
+}());
 var n = 20;
 var o = {
     name: "Leo",
     last_name: "Astros"
 };
 var t = [20, "tuple", true];
-var msg = "Number is ".concat(n, "\nPerson: ").concat(o.name, " ").concat(o.last_name, "\nTuple: ").concat(t);
+var a = ["one"];
+a[1] = "two";
+a.push("zero");
+var EnumName;
+(function (EnumName) {
+    EnumName[EnumName["value1"] = 0] = "value1";
+    EnumName[EnumName["value2"] = 1] = "value2";
+})(EnumName || (EnumName = {}));
+;
+var e = EnumName.value1;
+var msg = "Number is ".concat(n, "\nPerson: ").concat(o.name, " ").concat(o.last_name, "\nTuple: ").concat(t, "\nArray: ").concat(a, "\nEnum: ").concat(e);
 // Creating an element
 var element = document.createElement('h1');
 element.textContent = msg;
 // Appending element to the document
 document.body.appendChild(element);
+for (var i = 0; i < 5; i++) {
+    //document.body.appendChild(fName(i + 2));
+    var newElement = new childElement(i + 2);
+    document.body.appendChild(newElement.element());
+}
+function fName(n) {
+    var element = document.createElement("h".concat(n));
+    element.textContent = "text for child ".concat(n);
+    return element;
+}
--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
+class childElement {
+	private tagName: string;
+	private index: number;
+	constructor(n: number) {
+		this.index = n;
+		this.tagName = `h${n}`;
+	}
+	element():HTMLElement {
+		let el = document.createElement(this.tagName);
+		el.textContent = `text for child ${this.index}`;
+		return el;
+	}
+}
+
 let n : number = 20;
 let o: {
 	name: string,
@@ -7,13 +21,31 @@ let o: {
 	last_name: "Astros"
 };
 let t:[number, string, boolean] = [20, "tuple", true];
+let a:string[]=["one"];
+a[1] = "two";
+a.push("zero");
+enum EnumName {value1, value2};
+let e:EnumName = EnumName.value1;
+
 let msg: string = `Number is ${n}
 Person: ${o.name} ${o.last_name}
-Tuple: ${t}`;
+Tuple: ${t}
+Array: ${a}
+Enum: ${e}`;
 
 // Creating an element
 let element = document.createElement('h1');
 element.textContent = msg;
-
 // Appending element to the document
 document.body.appendChild(element);
+for(let i=0; i<5; i++) {
+	//document.body.appendChild(fName(i + 2));
+	let newElement = new childElement(i+2);
+	document.body.appendChild(newElement.element());
+}
+
+function fName(n: number): HTMLElement {
+	let element = document.createElement(`h${n}`);
+	element.textContent = `text for child ${n}`;
+	return element;
+}
