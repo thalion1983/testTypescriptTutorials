76f776f7391eb13a1c5e45716726f13158bbdebf
Leonardo Astros <leonardoastros@gmail.com>
2023-12-15 12:11:32 -0400

    Script executed on index.html

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 var msg = 'First message';
-console.log(msg);
+// Creating an element
+var element = document.createElement('h1');
+element.textContent = msg;
+// Appending element to the document
+document.body.appendChild(element);
--- app.ts
+++ app.ts
@@ -1,2 +1,8 @@
-let msg = 'First message';
-console.log(msg)
+let msg: string = 'First message';
+
+// Creating an element
+let element = document.createElement('h1');
+element.textContent = msg;
+
+// Appending element to the document
+document.body.appendChild(element);
